dc42dc8cda2931cf3f553cd7d86d721781a807a7
Jonathan Vitale <vitale.jonathan@ymail.com>
2021-01-07 15:00:58 +1100

    Add additional comment information about potential deadlock state

--- solution/solution-task-2.js
+++ solution/solution-task-2.js
@@ -14,15 +14,27 @@ This function is async! That means that invoking it will immediately
 return a Promise. Remember to use await if you want to wait for the
 result of the Promise.
 
-IMPORTANT ASSUMPTION! The robot with ID robotID used in this function
-has already been acquired. In other words, safeTeleportTo(<robotID>, <landmarkID>)
-is always used after gameController.acquireRobot(<robotID>).
-By acquiring <robotID> again in this function you will reach a deadlock state
-in the simulations!
 */
 
 async function safeTeleportTo(robotID, landmarkID){
     // You can remove/edit these lines of code
     gameController.log(`Calling safeTeleportTo('${robotID}', '${landmarkID}')`);
     await gameController.sleep(5000);
+    /*
+    IMPORTANT ASSUMPTION! The robot with ID robotID used in this function
+    has already been acquired outside this function.
+    In other words, safeTeleportTo(<robotID>, <landmarkID>)
+    is always used after let token = await gameController.acquireRobot(robotID).
+    If you use the command:
+    await gameController.acquireRobot(robotID);
+    inside this function, you will reach a deadlock state in task2 simulation.
+
+    However, you can use gameController.acquireRobot to acquire other robots
+    that you may need to safely move away before moving robotID to destination.
+    Also, remember that gameController.acquireRobot can be used with the second
+    parameter to false to prevent waiting forever if the robot that needs to be
+    acquired was already acquired. However, in this case, the returned token will
+    be null (so try to check about that later on when you are trying to release
+    the robot).
+    */
 }
