0b094727a28f894d515b8265dc2754c4489f5c7e
qf11bd <90132631+qf11bd@users.noreply.github.com>
2022-01-31 12:19:37 +1100

    Functional food delivery

--- solution/solution-task-5.js
+++ solution/solution-task-5.js
@@ -14,8 +14,145 @@ The function should return true if successful,
 or false otherwise.
 */
 
+// Function for delivering food, if else main statements to allow for robot being busy
+// Robot first checks if the seat is clean, and if the robot can grab food
+// If seat isn't clean, robot cleans before teleporting to the kitchen to grab the food
+// After serving the food, the total of that food is added to tbe bill total for that seat
+
 async function deliverFood(foodName, customerLocation){
-    // You can remove/edit these lines of code
     gameController.log(`Calling attendCustomerRequest('${customerLocation}', '${foodName}')`);
+    if (gameController.canAcquireRobot('green')) {
+        let token;
+        try{
+            let clean = await gameController.isSeatClean(customerLocation)
+            let grabFood = await gameController.canRobotGrabFood('green')
+            if ((clean !== false) && (grabFood !== false)) {
+                token = await gameController.acquireRobot('green', true);
+                safeTeleportTo('green', 'kitchen')
+                await gameController.sleep(10000);
+                gameController.takeFood('green')
+                await gameController.sleep(3000);
+                safeTeleportTo('green', customerLocation)
+                await gameController.sleep(10000);
+                gameController.serveFood('green')
+                await gameController.sleep(7000);
+            } else if (grabFood !== false) {
+                token = await gameController.acquireRobot('green', true);
+                safeTeleportTo('green', customerLocation)
+                await gameController.sleep(10000);
+                gameController.cleanSeat('green')
+                await gameController.sleep(5000);
+                safeTeleportTo('green', 'kitchen')
+                await gameController.sleep(10000);
+                gameController.takeFood('green', foodName)
+                await gameController.sleep(3000);
+                safeTeleportTo('green', customerLocation)
+                await gameController.sleep(10000);
+                gameController.serveFood('green')
+                await gameController.sleep(7000);
+            } else {
+                await gameController.sleep(10000)
+                deliverFood(foodName, customerLocation)
+            }
+        } catch(err){
+            console.log('Error with acquiring robot')
+        } finally {
+            foodPrice = gameController.getFoodPrice(foodName)
+            billTotal[customerLocation] = billTotal[customerLocation] + foodPrice
+            console.log(billTotal[customerLocation])
+            gameController.teleportRobotAtHome('green')
+            await gameController.sleep(2000)
+            gameController.releaseRobot('green', token);
+        }
+    } else if (gameController.canAcquireRobot('yellow')) {
+        let token;
+        try{
+            let clean = await gameController.isSeatClean(customerLocation)
+            let grabFood = await gameController.canRobotGrabFood('yellow')
+            if ((clean !== false) && (grabFood !== false)) {
+                token = await gameController.acquireRobot('yellow', true);
+                safeTeleportTo('yellow', 'kitchen')
+                await gameController.sleep(10000);
+                gameController.takeFood('yellow')
+                await gameController.sleep(3000);
+                safeTeleportTo('yellow', customerLocation)
+                await gameController.sleep(10000);
+                gameController.serveFood('yellow')
+                await gameController.sleep(7000);
+            } else if (grabFood !== false) {
+                token = await gameController.acquireRobot('yellow', true);
+                safeTeleportTo('yellow', customerLocation)
+                await gameController.sleep(10000);
+                gameController.cleanSeat('yellow')
+                await gameController.sleep(5000);
+                safeTeleportTo('yellow', 'kitchen')
+                await gameController.sleep(10000);
+                gameController.takeFood('yellow', foodName)
+                await gameController.sleep(3000);
+                safeTeleportTo('yellow', customerLocation)
+                await gameController.sleep(10000);
+                gameController.serveFood('yellow')
+                await gameController.sleep(7000);
+            } else {
+                await gameController.sleep(10000)
+                deliverFood(foodName, customerLocation)
+            }
+        } catch(err){
+            console.log('Error with acquiring robot')
+        } finally {
+            foodPrice = gameController.getFoodPrice(foodName)
+            billTotal[customerLocation] = billTotal[customerLocation] + foodPrice
+            console.log(billTotal[customerLocation])
+            gameController.teleportRobotAtHome('yellow')
+            await gameController.sleep(2000)
+            gameController.releaseRobot('yellow', token);
+        }
+    } else if (gameController.canAcquireRobot('purple')) {
+        let token;
+        try{
+            let clean = await gameController.isSeatClean(customerLocation)
+            let grabFood = await gameController.canRobotGrabFood('purple')
+            if ((clean !== false) && (grabFood !== false)) {
+                token = await gameController.acquireRobot('purple', true);
+                safeTeleportTo('purple', 'kitchen')
+                await gameController.sleep(10000);
+                gameController.takeFood('purple')
+                await gameController.sleep(3000);
+                safeTeleportTo('purple', customerLocation)
+                await gameController.sleep(10000);
+                gameController.serveFood('purple')
+                await gameController.sleep(7000);
+            } else if (grabFood !== false) {
+                token = await gameController.acquireRobot('purple', true);
+                safeTeleportTo('purple', customerLocation)
+                await gameController.sleep(10000);
+                gameController.cleanSeat('purple')
+                await gameController.sleep(5000);
+                safeTeleportTo('purple', 'kitchen')
+                await gameController.sleep(10000);
+                gameController.takeFood('purple', foodName)
+                await gameController.sleep(3000);
+                safeTeleportTo('purple', customerLocation)
+                await gameController.sleep(10000);
+                gameController.serveFood('purple')
+                await gameController.sleep(7000);
+            } else {
+                await gameController.sleep(10000)
+                deliverFood(foodName, customerLocation)
+            }
+        } catch(err){
+            console.log('Error with acquiring robot')
+        } finally {
+            foodPrice = gameController.getFoodPrice(foodName)
+            billTotal[customerLocation] = billTotal[customerLocation] + foodPrice
+            console.log(billTotal[customerLocation])
+            gameController.teleportRobotAtHome('purple')
+            await gameController.sleep(2000)
+            gameController.releaseRobot('purple', token);
+        }
+    } else {
+        await gameController.sleep(10000);
+        attendCustomerRequest(customerLocation, customerName)
+    }
     await gameController.sleep(5000);
 }
